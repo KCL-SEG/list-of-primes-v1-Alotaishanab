96c64fabc8e66454017e7936ff150632ca01db59
dozie9 <abdullah.alotaishan@kcl.ac.uk>
2022-12-09 07:49:27 +0100

    returns list of prime

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
 """List of prime numbers generator."""
+from math import sqrt
+
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
-    list = []
-    return list
+    list_of_primes = [2]
+    counter = 3
+    while len(list_of_primes) < number_of_primes:
+        # sqrta =
+        for i in list_of_primes:
+            if counter % i == 0:
+                break
+            if i >= sqrt(counter + 1):
+                list_of_primes.append(counter)
+                break
+        counter += 1
+    return list_of_primes
